577f035606f3adeaf9a29bd3c60e53af56980e02
scottrigby <scott@basekamp.com>
2011-05-15 23:27:53 -0400

    fix indentation & trailing whitespace from last patch

--- uc_order-wishlist.tpl.php
+++ uc_order-wishlist.tpl.php
@@ -256,15 +256,13 @@
                               <br />
                             </td>
                           </tr>
-                        
-<?php if($order->gift_wrap['chk'] && $order->gift_wrap['text']){ ?>
-	<tr>
-	  <td colspan="2">
-		<b><?php echo t('Gift card message:'); ?></b>&nbsp;<?php echo $order->gift_wrap['text']; ?>
-	  </td>
-	</tr> 
-<?php } ?>                        
-
+                          <?php if($order->gift_wrap['chk'] && $order->gift_wrap['text']){ ?>
+                        	<tr>
+                            <td colspan="2">
+                              <b><?php echo t('Gift card message:'); ?></b>&nbsp;<?php echo $order->gift_wrap['text']; ?>
+                            </td>
+                        	</tr>
+                          <?php } ?>
                           <?php }
                               }?>
                         </table>
